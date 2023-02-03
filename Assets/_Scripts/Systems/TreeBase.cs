1dad075951e53164a6c422fd30c97f4970ddb982
Esben Bjarnason <44700328+esbenlb@users.noreply.github.com>
2023-02-03 22:22:34 +0100

    basic trunk logic

--- Assets/_Scripts/Systems/TreeBase.cs
+++ Assets/_Scripts/Systems/TreeBase.cs
@@ -6,12 +6,13 @@ public class TreeBase : MonoBehaviour
 {
     public GameObject[] trunks;
     public GameObject[] crowns;
-    GameObject[] children;
+    List<Transform> children = new();
     void Start()
     {
-        
+        children.Add(transform.GetChild(0));
+        children[0].GetComponent<SpriteRenderer>().sprite = crowns[2].GetComponent<SpriteRenderer>().sprite;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
